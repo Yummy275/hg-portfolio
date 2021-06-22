7ed2b9813b1f5b061419cab7178b2b663bf76149
Yummy275 <htgonzalez94@yahoo.com>
2021-06-21 19:40:02 -0700

    homepage bg scrolling and content is centered

---
+++ src/components/HomeTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeTitle = () => {
+    return (
+        <h1 style={{ color: 'white' }}>
+            Hello. I'm Humberto Gonzalez. Web Developer.
+        </h1>
+    );
+};
+
+export default HomeTitle;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 import ScrollingBg from '../components/ScrollingBg';
+import HomeTitle from '../components/HomeTitle';
 import spaceBg from '../images/space-bg.png';
 
 const OverflowContainer = styled.div`
     height: 100vh;
     overflow: hidden;
+    position: relative;
+`;
+
+const Flex = styled.div`
+    position: absolute;
+    display: flex;
 `;
 
 const HomePage = () => {
     return (
         <OverflowContainer>
-            <ScrollingBg bg={spaceBg}>
-                <div>Hello</div>
-            </ScrollingBg>
+            <ScrollingBg bg={spaceBg}></ScrollingBg>
+            <Flex>
+                <HomeTitle></HomeTitle>
+            </Flex>
         </OverflowContainer>
     );
 };
