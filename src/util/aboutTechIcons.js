5ef6d8aa9f808a350752c208df6108dcdbe48964
Yummy275 <htgonzalez94@yahoo.com>
2021-07-01 15:49:07 -0700

    about tech icon object

---
+++ src/util/aboutTechIcons.js
@@ -0,0 +1,55 @@
+import reactIcon from '../images/tech_logos/react.png';
+import cssIcon from '../images/tech_logos/css3.png';
+import fbIcon from '../images/tech_logos/firebase.png';
+import htmlIcon from '../images/tech_logos/html5.png';
+import phaserIcon from '../images/tech_logos/phaser.png';
+import nodeIcon from '../images/tech_logos/nodejs.png';
+import styledIcon from '../images/tech_logos/sc.png';
+import twIcon from '../images/tech_logos/tailwind.png';
+import jsIcon from '../images/tech_logos/javascript.png';
+import bootIcon from '../images/tech_logos/bootstrap.png';
+
+const techLogos = {
+    react: {
+        name: 'React',
+        icon: { reactIcon },
+    },
+    css: {
+        name: 'Css',
+        icon: { cssIcon },
+    },
+    fb: {
+        name: 'Firebase',
+        icon: { fbIcon },
+    },
+    html: {
+        name: 'HTML',
+        icon: { htmlIcon },
+    },
+    phaser: {
+        name: 'Phaser',
+        icon: { phaserIcon },
+    },
+    express: {
+        name: 'Express JS',
+        icon: { nodeIcon },
+    },
+    styled: {
+        name: 'Styled-Components',
+        icon: { styledIcon },
+    },
+    tailwind: {
+        name: 'Tailwind',
+        icon: { twIcon },
+    },
+    js: {
+        name: 'Javascript',
+        icon: { jsIcon },
+    },
+    boot: {
+        name: 'Bootstrap',
+        icon: { bootIcon },
+    },
+};
+
+export default techLogos;
