fb9ba7122961a8d6ab593072f6a1d66a26e78da9
Yummy275 <htgonzalez94@yahoo.com>
2021-07-01 22:27:50 -0700

    about tech icons scroll action working

--- src/components/AboutTechIcon.js
+++ src/components/AboutTechIcon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useInView } from 'react-intersection-observer';
 
 const IconContainer = styled.div`
     text-align: center;
@@ -9,7 +10,11 @@ const IconContainer = styled.div`
 const Icon = styled.img`
     height: 3.2rem;
     width: 3.2rem;
+    transition: all 0.3s ease;
 
+    opacity: ${(props) => (props.visible ? '1' : '0')};
+    transform: ${(props) =>
+        props.visible ? 'scale(1) rotate(0)' : 'scale(.1) rotate(-360deg)'};
     @media (min-width: 640px) {
         height: 4rem;
         width: 4rem;
@@ -25,15 +30,21 @@ const Icon = styled.img`
 `;
 
 const IconName = styled.p`
+    transition: all 0.3s ease;
     color: white;
     font-style: italic;
+    opacity: ${(props) => (props.visible ? '1' : '0')};
 `;
 
 const AboutTechIcon = ({ name, icon }) => {
+    const [iconDiv, inView] = useInView({
+        threshold: 0.8,
+    });
+
     return (
-        <IconContainer>
-            <Icon src={icon} alt={name}></Icon>
-            <IconName>{name}</IconName>
+        <IconContainer ref={iconDiv}>
+            <Icon src={icon} alt={name} visible={inView}></Icon>
+            <IconName visible={inView}>{name}</IconName>
         </IconContainer>
     );
 };
